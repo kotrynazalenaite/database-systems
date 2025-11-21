723c80397ac90d904744a206337df7b1c241dc03
kotrynazalenaite <118669439+kotrynazalenaite@users.noreply.github.com>
2025-11-21 08:23:49 +0200

    homework3
    changed the last line

--- homework3/app.py
+++ homework3/app.py
@@ -95,4 +95,4 @@ def recipe_detail(recipe_id):
 # --- 3. Run the Application ---
 if __name__ == '__main__':
     # Use PORT environment variable for deployment (Render), default to 5000 locally
-    app.run(debug=True, port=int(os.environ.get('PORT', 5010)))
+    app.run(debug=True, host = "0.0.0.0", port = "8080")
